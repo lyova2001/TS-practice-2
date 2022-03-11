f9b08616b0357e713d775463f260290b13ba46be
Lyova-FC <lyovaavetisyan31@gmail.com>
2022-03-11 16:14:52 +0300

    Form handler

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+const form = document.querySelector('form')!
+const addressInput = document.getElementById('address')! as HTMLInputElement
+
+const searchAddressHandler = (event: Event) => {
+  event.preventDefault()
+  const enteredAddress = addressInput.value
+
+  console.log(enteredAddress)
+}
+
+form.addEventListener('submit', searchAddressHandler)
